bde1023af94e3816215a4d6a496cfa56b336d1d6
Lenovo Y520 <mohammadnemer1996@hotmail.com>
2020-02-07 14:49:40 +0200

    four

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,33 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
 
   constructor(http : HttpClient) {
-    for(let i = 1;i <= 100;i++){
-      http.get(`https://bugzilla.mozilla.org/rest/bug/${i}/history`).subscribe(data=>{
+
+    var idBug = [296,
+      326,
+      336,
+      424,
+      491,
+      518,
+      839,
+      1299,
+      2042,
+      2994,
+      3150,
+      4491,
+      4765,
+      4990,
+      5754,
+      5863,
+      6001,
+      7535,
+      7816,
+      8163,
+      8800,
+      11226,
+      11494];
+
+    for(let i = 0;i < 23;i++){
+      http.get(`https://bugzilla.mozilla.org/rest/bug/${idBug[i]}/history`).subscribe(data=>{
         console.log(data);
         this.ProcessExcel2(data);
     })
@@ -210,7 +235,32 @@ fileUpload2() {
       }
 }
 ProcessExcel2(data) {
+  let whoName = {'afranke@mathweb.org':{color:'red'},'agracebush@netscape.net':{color:'blue'},
+  'alecf@flett.org':{color:'green'},'benc@meer.net':{color:'yellow'},
+  'briano@bluemartini.com':{color:'pink'},'cathleennscp@netscape.net':{color:'#1abc9c'},
+  'chofmann@gmail.com':{color:'#9b59b6'},'Chris.Yeh@nokia.com':{color:'#32ff7e'},
+  'cpratt@formerly-netscape.com.tld':{color:'#34495e'},'dbaron@dbaron.org':{color:'#474787'},
+  'desale@formerly-netscape.com.tld':{color:'#f1c40f'},'don@formerly-netscape.com.tld':{color:'#84817a'},
+  'doug.turner@gmail.com':{color:'#f39c12'},'elig@formerly-netscape.com.tld':{color:'#227093'},
+  'endico@mozilla.org':{color:'#d35400'},'esther@formerly-netscape.com.tld':{color:'#ccae62'},
+  'hangas@formerly-netscape.com.tld':{color:'#e67e22'},'harleyangel@angelfire.com':{color:'#ffda79'},
+  'jar@formerly-netscape.com.tld':{color:'#2980b9'},'jaymoz@gmail.com':{color:'#b33939'},
+  'jensend@iname.com':{color:'#e74c3c'},'jimmykenlee@yahoo.com':{color:'#40407a'},
+  'jonasj@qio.dk':{color:'#7f8c8d'},'jruderman@gmail.com':{color:'#6F1E51'},
+  'kmcclusk@formerly-netscape.com.tld':{color:'#2c3e50'},'laurel@formerly-netscape.com.tld':{color:'#833471'},
+  'law@formerly-netscape.com.tld':{color:'#fd79a8'},'lchiang@formerly-netscape.com.tld':{color:'#B53471'},
+  'leger@formerly-netscape.com.tld':{color:'#e17055'},'mcafee@gmail.com':{color:'#ED4C67'},
+  'michaell@formerly-netscape.com.tld':{color:'#fdcb6e'},'mike.rolig@writeme.com':{color:'#FDA7DF'},
+  'mozilla@davidkrause.com':{color:'#55efc4'},'msanz@formerly-netscape.com.tld':{color:'#D980FA'},
+  'myk@mykzilla.org':{color:'#74b9ff'},'phil@formerly-netscape.com.tld':{color:'#9980FA'},
+  'rickg@formerly-netscape.com.tld':{color:'#81ecec'},'pkwarren@gmail.com':{color:'#5758BB'},
+  'rods@formerly-netscape.com.tld':{color:'#18dcff'},'roland.mainz@nrubsig.org':{color:'#1B1464'},
+  'rubydoo123@aol.com':{color:'#FFC312'},'scottputterman@gmail.com':{color:'#C4E538'},
+  'selmer@formerly-netscape.com.tld':{color:'#F79F1F'},'sfraser_bugs@smfr.org':{color:'#A3CB38'},
+  'tever@formerly-netscape.com.tld':{color:'#EE5A24'},'timeless@bemail.org':{color:'#009432'},
+  'vdwel@caiw.nl':{color:'#006266'}
 
+    };
   google.charts.load('current', {packages: ['timeline']});
   google.charts.setOnLoadCallback(drawChart);
   function drawChart() {
@@ -223,14 +273,19 @@ ProcessExcel2(data) {
         dataTable.addColumn({ type: 'date', id: 'Start' });
         dataTable.addColumn({ type: 'date', id: 'End' });
         const arr = [];
+        let options = {
+          colors: []
+        };
         data.bugs[0].history.forEach(element => {
           element.changes.forEach(elementx => {
             if (elementx.field_name == 'status') {
               arr.push({who: element.who, when: element.when,status:elementx.added});
+              options.colors.push(whoName[element.who]?whoName[element.who].color:`${getRandomColor()}`);
             }
           });
 
         });
+
         console.log(arr);
         arr.forEach((ele, index) => {
           if (index != arr.length - 1 ) {
@@ -240,8 +295,15 @@ ProcessExcel2(data) {
             dataTable.addRow([ele.status,ele.who,new Date(ele.when),new Date(ele.when)]);
           }
         });
-
-        chart.draw(dataTable);
+        function getRandomColor() {
+          var letters = '0123456789ABCDEF';
+          var color = '#';
+          for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+          }
+          return color;
+        }
+        chart.draw(dataTable,options);
       }
 
 
